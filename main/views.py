63856ab1429b87cb14bc96849624072e594b774b
David de Lucas <delucas.david@gmail.com>
2020-04-15 18:47:36 +0200

    WIP: css frmwork intial usage

--- main/views.py
+++ main/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import BusinessCase
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
+
 # Create your views here.
 def homepage(request):
     return render(request = request,
@@ -15,12 +18,15 @@ def register(request):
         if form.is_valid():
             user = form.save()
             username = form.cleaned_data.get('username')
+            messages.success(request, f"New Account created: {username}")
+            username = form.cleaned_data.get('username')
             login(request, user)
+            messages.info(request, f"You are now logged in as: {username}")
             return redirect("main:homepage")
 
         else:
             for msg in form.error_messages:
-                print(form.error_messages[msg])
+                messages.error(request, f"{msg}: {form.error_messages[msg]}")
 
             return render(request = request,
                           template_name = "main/register.html",
